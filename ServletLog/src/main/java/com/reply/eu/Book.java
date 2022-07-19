dbc3a4cccde6841bf8e683986054ee338c163e4e
ReplyTeamAngelaS <replyteam.angelas@gmail.com>
2022-07-19 10:45:42 +0300

    Update

--- ServletLog/src/main/java/com/reply/eu/Book.java
+++ ServletLog/src/main/java/com/reply/eu/Book.java
@@ -1,10 +1,54 @@
 package com.reply.eu;
 
+import java.util.*;
+
 public class Book {
 	
+	private int bookID;
 	private String isbn;
 	private String authorName;
 	private String publisher;
+	
+	public Book(int bookID,String isbn, String authorName, String publisher) {
+		this.bookID=bookID;
+		this.isbn=isbn;
+		this.authorName=authorName;
+		this.publisher=publisher;
+	}
+	
+	public String getIsbn() {
+		return this.isbn;
+	}
+	
+	public String getAuthorName() {
+		return this.authorName;
+	}
+	
+	public String getPublisher() {
+		return this.publisher;
+	}
+	
+	public int getID() {
+		return this.bookID;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn=isbn;
+	}
+	
+	public void setAuthorName(String authorName) {
+		this.authorName=authorName;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher=publisher;
+	}
+	
+	public void setID(int bookID) {
+		this.bookID=bookID;
+	}
+	
+	
 	
 
 }
--- ServletLog/src/main/java/com/reply/eu/BooksServlet.java
+++ ServletLog/src/main/java/com/reply/eu/BooksServlet.java
@@ -6,35 +6,57 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import javax.servlet.annotation.WebServlet;
 
-/**
- * Servlet implementation class BooksServlet
- */
 //@WebServlet("/books")
 public class BooksServlet extends HttpServlet {
 	
-	public static String BOOK="Book 1";
+	private static List<Book> listBooks=new ArrayList<Book>();
+	private int CONTOR=0;
 
-	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append(BOOK);
+		Book book=new Book(++CONTOR,request.getParameter("isbn"), request.getParameter("authorName"), request.getParameter("publisher"));
+		listBooks.add(book); 
+		for(Book b:listBooks) {
+			response.getWriter().println("Book #"+ b.getID() +" isbn= "+b.getIsbn()+" "+" authorName= "+b.getAuthorName()+" "+" publisher= "+b.getPublisher());
+			}
 	}
 
-	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doGet(request,response);
+		
 	}
 
-	
+	@Override
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BOOK="Book 2";
-		doGet(request, response);
+		int contor=Integer.parseInt(request.getParameter("ID"));
+		String changeIsbn=request.getParameter("changeIsbnWith");
+		String changeAuthorName=request.getParameter("changeAuthorNameWith");
+		String changePublisher=request.getParameter("changePublisherWith");
+		for(Book book:listBooks) {
+			if(contor==book.getID()) {
+				book.setIsbn(changeIsbn);
+				book.setAuthorName(changeAuthorName);
+				book.setPublisher(changePublisher);
+			}
+		}
+		for(Book b:listBooks) {
+			response.getWriter().println("Book #"+ b.getID() +" isbn= "+b.getIsbn()+" "+" authorName= "+b.getAuthorName()+" "+" publisher= "+b.getPublisher());
+			}
 	}
 
-	
+	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BOOK="";
-		doGet(request, response);
+		int contor=Integer.parseInt(request.getParameter("ID"));
+		
+		for(Book book:listBooks) {
+			if(contor==book.getID()) {
+				this.listBooks.remove(book);
+			}
+		}
 	}
 
 }
