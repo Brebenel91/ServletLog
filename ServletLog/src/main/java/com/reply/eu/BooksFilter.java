bfa6b9e81464ba1abb2bdf351aaca7c846866d6a
Andrei Nicolae <ReplyTeam.AndreiN@gamil.com>
2022-07-19 11:26:52 +0300

    Created tomcat 9 branch

--- ServletLog/src/main/java/com/reply/eu/BooksFilter.java
+++ ServletLog/src/main/java/com/reply/eu/BooksFilter.java
@@ -2,17 +2,18 @@ package com.reply.eu;
 
 import java.io.IOException;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 
 
 public class BooksFilter extends HttpFilter implements Filter {
--- ServletLog/src/main/java/com/reply/eu/BooksServlet.java
+++ ServletLog/src/main/java/com/reply/eu/BooksServlet.java
@@ -2,15 +2,13 @@ package com.reply.eu;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class BooksServlet extends HttpServlet {
 
